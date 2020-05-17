56fc74c49da2996ba53a34ea5732e39e1406fcf3
MEAN Hive <meanhive@zoho.com>
2020-05-17 14:04:37 +0530

    Update environment.ts

--- Angularfirebase/src/environments/environment.ts
+++ Angularfirebase/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyD5rZmixwFDkJ99eKc5gyg7tWUEnQCOwvw',
-    authDomain: 'auth-angular-e6381.firebaseapp.com',
-    databaseURL: 'https://auth-angular-e6381.firebaseio.com',
-    projectId: 'auth-angular-e6381',
-    storageBucket: 'auth-angular-e6381.appspot.com',
-    messagingSenderId: '880031353108',
-    appId: '1:880031353108:web:998ed6eb800f9b91509a01',
-    measurementId: 'G-CWB7MJ3DQC',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   },
 };
 
